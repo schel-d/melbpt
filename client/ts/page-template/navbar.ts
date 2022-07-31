d97bf685750253636c2b52060443a63547e1a611
schel-d <schel.d.dev@gmail.com>
2022-07-31 19:20:37 +1000

    rudimentary search implemented

--- client/ts/page-template/navbar.ts
+++ client/ts/page-template/navbar.ts
@@ -1,3 +1,9 @@
+import { getNetwork } from "../network";
+import { search, SearchOption, searchOptionsWholeSite } from "./search";
+
+const searchEmptyMessage = "Results will appear here.";
+const searchNoResultsMessage = "No results.";
+
 type NavbarElements = {
   navbarBg: HTMLElement,
   menuButton: HTMLElement,
@@ -6,6 +12,7 @@ type NavbarElements = {
   menu: HTMLElement,
   search: HTMLElement,
   searchInput: HTMLInputElement,
+  searchResults: HTMLElement
 }
 type NavbarState = {
   blendMode: boolean,
@@ -31,7 +38,8 @@ function getNavbarElements(): NavbarElements {
     iconSearchButton: getElementOrThrow("navbar-search-icon-button"),
     menu: getElementOrThrow("navbar-expandable-menu-container"),
     search: getElementOrThrow("navbar-expandable-search-container"),
-    searchInput: getInputOrThrow("navbar-expandable-search-input")
+    searchInput: getInputOrThrow("navbar-expandable-search-input"),
+    searchResults: getElementOrThrow("navbar-expandable-search-results")
   };
 }
 
@@ -59,6 +67,8 @@ export function initNavbar() {
   });
 
   updateBlendMode(elements, state);
+
+  initSearch(elements);
 }
 
 function updateBlendMode(elements: NavbarElements, state: NavbarState) {
@@ -99,4 +109,95 @@ function toggleSearch(elements: NavbarElements, state: NavbarState) {
 function setClass(element: HTMLElement, className: string, value: boolean) {
   if (value) { element.classList.add(className); }
   else { element.classList.remove(className); }
+}
+
+function initSearch(elements: NavbarElements) {
+  let latestChangeID = 0;
+  let networkHash: string | null = null;
+  let options: SearchOption[] | null = null;
+
+  searchMessage(searchEmptyMessage, elements);
+
+  elements.searchInput.addEventListener("input", () => {
+    const query = elements.searchInput.value;
+    const changeID = latestChangeID + 1;
+    latestChangeID = changeID;
+
+    getNetwork()
+      .then(network => {
+        // If by the time this promise resolves the input has changed, then do
+        // nothing (otherwise we'll be competing with that promise).
+        if (latestChangeID != changeID) { return; }
+
+        if (network.hash != networkHash || options == null) {
+          networkHash = network.hash;
+          options = searchOptionsWholeSite(network);
+        }
+
+        if (query.length == 0) {
+          searchMessage(searchEmptyMessage, elements);
+          return;
+        }
+
+        const results = search(query, options);
+
+        if (results.length == 0) {
+          searchMessage(searchNoResultsMessage, elements);
+          return;
+        }
+
+        searchResults(results, elements);
+      })
+      .catch(() => {
+        // If by the time this promise resolves the input has changed, then do
+        // nothing (otherwise we'll be competing with that promise).
+        if (latestChangeID != changeID) { return; }
+        console.log("fail");
+      });
+  });
+}
+
+function searchMessage(message: string, elements: NavbarElements) {
+  const noResults = document.createElement("p");
+  noResults.className = "message";
+  noResults.textContent = message;
+
+  elements.searchResults.replaceChildren(noResults);
+}
+function searchResults(results: SearchOption[], elements: NavbarElements) {
+  const resultsHTML = results.map(r => {
+    const result = document.createElement("a");
+    result.className = "result";
+    result.href = r.url;
+
+    const icon = document.createElement("span");
+    icon.className = "iconify";
+    icon.dataset.icon = r.icon;
+
+    const column = document.createElement("div");
+    column.className = "column";
+
+    const titleWrapper = document.createElement("div");
+    titleWrapper.className = "one-line";
+    const title = document.createElement("p");
+    title.className = "title";
+    title.textContent = r.title;
+    titleWrapper.append(title);
+    column.append(titleWrapper);
+
+    if (r.subtitle != null) {
+      const subtitleWrapper = document.createElement("div");
+      subtitleWrapper.className = "one-line";
+      const subtitle = document.createElement("p");
+      subtitle.className = "subtitle";
+      subtitle.textContent = r.subtitle;
+      subtitleWrapper.append(subtitle);
+      column.append(subtitleWrapper);
+    }
+
+    result.append(icon, column);
+    return result;
+  });
+
+  elements.searchResults.replaceChildren(...resultsHTML);
 }
--- client/ts/page-template/search.ts
+++ client/ts/page-template/search.ts
@@ -1,9 +1,73 @@
+import { Network } from "../network";
+
 export type SearchOption = {
   title: string,
   subtitle: string | null,
+  icon: string,
   url: string,
+
+  // Todo: Search tags, e.g. typing "Traralgon" should still bring up
+  // "Gippsland line" imo.
 }
 
-function search(inputs: SearchOption[]): SearchOption[] {
+export function searchOptionsStopsAndLines(network: Network): SearchOption[] {
+  const options: SearchOption[] = [];
 
+  options.push(...network.stops.map(s => {
+    return {
+      title: `${s.name} Station`,
+      subtitle: "sumit",
+
+      // Alternatives: "uil:map-marker", "uil:map-pin"
+      icon: "ph:train-simple-bold",
+
+      url: `/${s.urlName}`
+    };
+  }));
+
+  options.push(...network.lines.map(l => {
+    return {
+      title: `${l.name} Line`,
+      subtitle: "sumit",
+
+      // Alternative: "uil:code-branch"
+      icon: "uil:slider-h-range",
+
+      url: `/lines/${l.id.toFixed()}`
+    };
+  }));
+
+  return options;
+}
+
+export function searchOptionsWholeSite(network: Network): SearchOption[] {
+  const options = searchOptionsStopsAndLines(network);
+
+  options.push({
+    title: "Lines",
+    subtitle: null,
+    icon: "uil:code-branch",
+    url: "/lines"
+  });
+  options.push({
+    title: "About",
+    subtitle: null,
+    icon: "uil:info-circle",
+    url: "/about"
+  });
+  options.push({
+    title: "Settings",
+    subtitle: null,
+    icon: "uil:setting",
+    url: "/settings"
+  });
+
+  return options;
+}
+
+export function search(query: string, inputs: SearchOption[]): SearchOption[] {
+  const results = inputs.filter(i =>
+    i.title.toLowerCase().startsWith(query.toLowerCase()));
+
+  return results.slice(0, 5);
 }
