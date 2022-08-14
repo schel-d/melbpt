018d4a65b19ed328b3dd68abc811032e0d77efdb
schel-d <schel.d.dev@gmail.com>
2022-08-14 13:33:06 +1000

    js/css splitting + stop page

--- client/ts/bundles/index.ts
+++ client/ts/bundles/index.ts
@@ -1,10 +1,7 @@
 import { getCanvasOrThrow, getElementOrThrow, getInputOrThrow } from "../dom-utils";
 import { initHeroBG } from "../hero-bg";
-import { initNavbar } from "../page-template/navbar";
 import { searchOptionsStops } from "../page-template/search";
 import { displayResults, initSearch } from "../page-template/search-ui";
-
-initNavbar();
 
 const mainSearchInput = getInputOrThrow("main-search-input");
 const mainSearchForm = getElementOrThrow("main-search-form");
--- ts-server/main.ts
+++ ts-server/main.ts
@@ -1,5 +1,36 @@
 import express from "express";
 import compression from "compression";
+import { fetchNetwork, Network } from "./network";
+
+/**
+ * How often (in milliseconds) to re-download the network data from the api.
+ * Currently set to the value of every 30 minutes.
+ */
+const dataRefreshIntervalMs = 30 * 60 * 1000;
+
+const reservedRoutes = [
+  "/",
+  "/about",
+  "/css",
+  "/dev",
+  "/disruption",
+  "/go",
+  "/home",
+  "/html",
+  "/icons",
+  "/img",
+  "/journey",
+  "/js",
+  "/lines",
+  "/map",
+  "/search",
+  "/service",
+  "/settings",
+  "/train"
+];
+
+let network: Network | null = null;
+let stopUrls: string[] | null = null;
 
 export async function main() {
   console.log("Starting...");
@@ -13,16 +44,74 @@ export async function main() {
   app.set("view engine", "pug");
   app.use(express.static(".out/public"));
 
+  try {
+    setupNetwork(await fetchNetwork());
+    registerRoutes(app);
+  }
+  catch {
+    console.error("Failed to fetch initial network information.");
+
+    app.all('*', (_req, res: express.Response) => {
+      res.sendStatus(500);
+    });
+  }
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+  });
+}
+
+function registerRoutes(app: express.Application) {
   app.get("/", (_req, res: express.Response) => {
     res.render("index");
   });
 
   // 404 page
-  app.all('*', (_req, res: express.Response) => {
+  app.all('*', (req: express.Request, res: express.Response) => {
+    if (stopUrls?.includes(req.url)) {
+      res.render("stop");
+      return;
+    }
     res.status(404).render("404");
   });
+}
 
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}.`);
-  });
+function setupNetwork(incoming: Network) {
+  const firstTime = network == null;
+
+  // Check the incoming network information to make sure no reserved routes are
+  // being used by the stops custom url, e.g. no stop can have url "/js".
+  const incomingStopUrls = incoming.stops.map(s => `/${s.urlName}`);
+  const badRoute = reservedRoutes.find(r => incomingStopUrls.includes(r));
+  if (badRoute != null) {
+    throw new Error(
+      `The route ${badRoute} wasn't not being reserved correctly.`
+    );
+  }
+
+  // Set the global variables (note that this only happens if there's no route
+  // clashes).
+  network = incoming;
+  stopUrls = incomingStopUrls;
+
+  if (!firstTime) {
+    console.log(`Refreshed data (network hash="${network.hash}").`);
+    return;
+  }
+
+  console.log(`Downloaded data (network hash="${network.hash}").`);
+
+  // Every 30 minutes re-download the data from the api to stay up to date.
+  // If an error occurs, continue using the previous version of the data,
+  // and try again in another 30 minutes.
+  setInterval(async () => {
+    try {
+      setupNetwork(await fetchNetwork());
+    }
+    catch (ex) {
+      // If an error occurs, just log it. The variable will still contain
+      // the old data, so no need to touch it.
+      console.log(`Error refreshing data (will continue using old data).`, ex);
+    }
+  }, dataRefreshIntervalMs);
 }
